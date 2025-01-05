093593039ed115e0064cf33752fd69fbb067685d
valenciavannessa <119060608+valenciavannessa@users.noreply.github.com>
2025-01-05 23:53:44 +0700

    Add vite config

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import NotesApp from './components/NotesApp';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import NotesApp from "./components/NotesApp";
 
-import './styles/style.css';
+import "./styles/style.css";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(<NotesApp />);
